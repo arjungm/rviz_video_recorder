06d26171c8cae7f8021bad7ad23d67203831ff1e
arjungm <ajn.menon@gmail.com>
2014-08-14 15:39:24 -0700

    added a button to start saving images from window

--- include/rviz_video_recorder/recorder_panel.h
+++ include/rviz_video_recorder/recorder_panel.h
@@ -1,9 +1,14 @@
 #ifndef RECORDER_PANEL_H
 #define RECORDER_PANEL_H
 
+#include <queue>
+
 #include <ros/ros.h>
 #include <rviz/panel.h>
+
 #include <QLineEdit>
+#include <QPushButton>
+#include <QPixmap>
 
 namespace rviz_recorder
 {
@@ -16,15 +21,23 @@ namespace rviz_recorder
       virtual void load( const rviz::Config& config );
       virtual void save(rviz::Config config ) const;
       void setTopic( const QString& topic );
+      virtual void onInitialize();
     
     protected Q_SLOTS:
       void updateTopic();
+      void saveSnapshotToQueue();
+      void toggleRecording();
 
     protected:
       ros::NodeHandle nh_;
       ros::Subscriber command_listener_;
       QString command_topic_;
       QLineEdit* command_topic_editor_;
+
+      QPushButton* record_button_;
+
+      std::queue<QPixmap> snapshot_queue_;
+      bool is_recording_;
   };
 }
 
--- src/recorder_panel.cpp
+++ src/recorder_panel.cpp
@@ -1,22 +1,41 @@
 #include "rviz_video_recorder/recorder_panel.h"
 
+#include <rviz/visualization_manager.h>
+
 #include <QLabel>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 
 namespace rviz_recorder
 {
   RecorderPanel::RecorderPanel( QWidget* parent ) : 
-    rviz::Panel( parent )
+    rviz::Panel( parent ),
+    is_recording_(false)
   {
+    record_button_ = new QPushButton("Start Recording");
+
     QHBoxLayout* topic_layout = new QHBoxLayout;
-    topic_layout->addWidget( new QLabel( "Output Topic:" ));
+    topic_layout->addWidget( new QLabel( "Command Topic:" ));
     command_topic_editor_ = new QLineEdit;
     topic_layout->addWidget( command_topic_editor_ );
-    setLayout( topic_layout );
+    
+    // outermost
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addLayout( topic_layout );
+    layout->addWidget( record_button_ );
 
+    setLayout( layout );
+
+    ROS_INFO("Constructed!");
+  }
+
+  void RecorderPanel::onInitialize()
+  {
     connect( command_topic_editor_ , SIGNAL( editingFinished() ), this, SLOT( updateTopic() ));
+    connect( record_button_, SIGNAL( clicked() ), this, SLOT( toggleRecording() ) );
+    connect( vis_manager_, SIGNAL( preUpdate() ), this, SLOT( saveSnapshotToQueue() ));
 
-    ROS_INFO("Completed setting up the panel!");
+    ROS_INFO("Initialized!");
   }
 
   void RecorderPanel::load( const rviz::Config& config )
@@ -52,6 +71,22 @@ namespace rviz_recorder
       {
       }
       Q_EMIT configChanged();
+    }
+  }
+  void RecorderPanel::toggleRecording()
+  {
+    is_recording_ = !is_recording_;
+    ROS_INFO("Recording is %s.", is_recording_?"started":"stopped");
+    if(is_recording_)
+      record_button_->setText("Stop Recording");
+    else
+      record_button_->setText("Start Recording");
+  }
+  void RecorderPanel::saveSnapshotToQueue()
+  {
+    if(is_recording_)
+    {
+      ROS_INFO("Recording!");
     }
   }
 }
